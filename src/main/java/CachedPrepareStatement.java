a63a62e938bcc3f81ec2d66deeac1bf869a038fd
Tahir Iqbal <mtahiriqbaldeveloper@gmail.com>
2025-07-22 13:36:35 +0200

    working on statement cache

--- src/main/java/CachedPrepareStatement.java
+++ src/main/java/CachedPrepareStatement.java
@@ -1,2 +1,35 @@
-public class CachedPrepareStatement {
+import api.ReleasableStatement;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CachedPrepareStatement extends DelegatingPreparedStatementAdapter implements ReleasableStatement {
+
+    private final StatementCacheConnection connection;
+    private final Object key;
+    private long lastTimeUsed;
+    private AtomicBoolean closed = new AtomicBoolean(false);
+
+    public CachedPrepareStatement(PreparedStatement statement, StatementCacheConnection connection, Object key) {
+        super(statement);
+        this.connection = connection;
+        this.key = key;
+    }
+
+    @Override
+    public void release() throws SQLException {
+        getWrappedStatement().close();
+    }
+    @Override
+    public long getLastTimeUsed() {
+        return lastTimeUsed;
+    }
+
+    public void setLastTimeUsed(long lastTimeUsed) {
+        this.lastTimeUsed = lastTimeUsed;
+    }
+    public void setOpened(){
+        closed.set(false);
+    }
 }
